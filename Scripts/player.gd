644fc7c436b7b0aa4ebc881a0c4559d41adb7868
Asvath003 <asvathm999@gmail.com>
2026-08-31 22:44:55 +0530

    Add player movement script

---
+++ Scripts/player.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+const SPEED = 250.0
+
+func _physics_process(_delta):
+	var input_dir = Vector2.ZERO
+
+	if Input.is_key_pressed(KEY_A) or Input.is_key_pressed(KEY_LEFT):
+		input_dir.x -= 1
+	if Input.is_key_pressed(KEY_D) or Input.is_key_pressed(KEY_RIGHT):
+		input_dir.x += 1
+	if Input.is_key_pressed(KEY_W) or Input.is_key_pressed(KEY_UP):
+		input_dir.y -= 1
+	if Input.is_key_pressed(KEY_S) or Input.is_key_pressed(KEY_DOWN):
+		input_dir.y += 1
+
+	velocity = input_dir.normalized() * SPEED
+	move_and_slide()
